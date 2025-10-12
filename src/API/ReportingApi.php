fa799b461186e3fbe47d9f65e74664e7f8fcf178
Islam Abdelfattah <IslamM.Abdelfattah@gmail.com>
2025-10-12 20:35:09 +0300

    Introduce reporting and clearance APIs

---
+++ src/API/ReportingApi.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Zid\Zatca\API;
+
+class ReportingApi implements ZatcaApiInterface
+{
+    public function __construct(protected ZatcaClient $client)
+    {
+    }
+
+    public function single(string $binarySecurityToken, string $secret, string $invoiceHash, string $uuid, string $invoiceXml, string $language = 'en'): array
+    {
+        return $this->client->postRequest('invoices/reporting/single', [
+            "invoiceHash" => $invoiceHash,
+            "uuid" => $uuid,
+            "invoice" => base64_encode($invoiceXml),
+        ], [
+            'Authorization' => 'Basic ' . base64_encode(
+                "$binarySecurityToken:$secret"
+            ),
+
+            'Accept-Language' => $language,
+            'Content-Type' => 'application/json',
+            'Clearance-Status' => '1',
+        ]);
+    }
+}
